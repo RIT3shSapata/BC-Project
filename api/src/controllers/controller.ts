3854b96dfd604fd172b4b3991c6af4a1b1462b35
Ritesh Sapata <riteshsapata@gmail.com>
2022-05-01 13:24:13 +0530

    cryptography functions added

--- api/src/controllers/controller.ts
+++ api/src/controllers/controller.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express';
 import { DocumentData } from '../types/DocumentData';
 import { write, read, searchMeta } from '../utils/bigChainDB';
+import {
+    encrypt,
+    generateKeys,
+    decrypt,
+    createSignature,
+    verifySignature,
+} from '../utils/cryptography';
 
 const addData = async (req: Request, res: Response) => {
     const { personalData, id }: DocumentData = req.body;
@@ -20,4 +27,44 @@ const findByMetaData = async (req: Request, res: Response) => {
     res.send(transaction);
 };
 
-export { addData, getData, findByMetaData };
+const encryptionTest = async (req: Request, res: Response) => {
+    const { plainText, publicKey } = req.body;
+    const cipherText = encrypt(JSON.stringify(plainText), publicKey);
+    res.send(cipherText);
+};
+
+const decryptionTest = async (req: Request, res: Response) => {
+    const { cipherText, privateKey } = req.body;
+    const plainText = decrypt(cipherText, privateKey);
+    res.send(plainText);
+};
+
+const generateKeyPair = async (req: Request, res: Response) => {
+    res.send(generateKeys());
+};
+
+const generateSignature = async (req: Request, res: Response) => {
+    const { plainText, privateKey } = req.body;
+    const signature = createSignature(JSON.stringify(plainText), privateKey);
+    res.send(signature);
+};
+
+const verifySign = async (req: Request, res: Response) => {
+    const { plainText, signature, publicKey } = req.body;
+    const verified = verifySignature(
+        JSON.stringify(plainText),
+        signature,
+        publicKey
+    );
+    res.send(verified);
+};
+export {
+    addData,
+    getData,
+    findByMetaData,
+    encryptionTest,
+    generateKeyPair,
+    decryptionTest,
+    generateSignature,
+    verifySign,
+};
